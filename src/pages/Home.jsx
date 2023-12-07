a65684e5307de6a1219d735bd28454c27c4b3413
Franco Tevez <tevezzfranco@gmail.com>
2023-12-07 18:27:53 -0300

    intentando arreglar AOS 4

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,7 @@
-import { useEffect } from "react";
+
 import Body from "../containers/Body"
-import { useLocation } from 'react-router-dom';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
 
 function Home() {
-  const location = useLocation();
-
-  useEffect(() => {
-    setTimeout(() => {
-      AOS.init();
-      AOS.refreshHard(); 
-    }, 200);
-  }, [location.pathname]);
 
   return (
     <>
