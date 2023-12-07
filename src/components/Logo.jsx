ec1417b61fd46e5619953ffaf6edfb021bf530e4
Franco Tevez <tevezzfranco@gmail.com>
2023-12-07 18:32:12 -0300

    arreglando AOS 5

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,10 +1,15 @@
 import {Link} from "react-router-dom"
 import logo from "../assets/logoPangea.png"
 import '../styles/navbar.css'
+import AOS from "aos"
+import 'aos/dist/aos.css';
 
 function Logo() {
   const scrollToTop = () => {
     window.scrollTo(0, 0);
+    setTimeout(() => {
+      AOS.refresh();
+    }, 1000);
   };
 
   return (
