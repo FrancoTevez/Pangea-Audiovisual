3bc2b640b71ed0a0973b8a3b5c146ff5eb062c9b
Franco Tevez <tevezzfranco@gmail.com>
2023-12-07 18:38:06 -0300

    arreglando AOS 7

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,19 @@
-
+import { useEffect } from "react";
 import Body from "../containers/Body"
+import { useLocation } from 'react-router-dom';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 function Home() {
+  const location = useLocation();
+
+  useEffect(() => {
+    setTimeout(() => {
+      AOS.init();
+      document.querySelectorAll("img").forEach((img) => img.addEventListener("load", () => AOS.refresh()))
+      document.querySelectorAll("video").forEach((video) => video.addEventListener("load", () => AOS.refresh()))
+    }, 1000);
+  }, [location.pathname]);
 
   return (
     <>
--- src/pages/Nosotros.jsx
+++ src/pages/Nosotros.jsx
@@ -1,7 +1,20 @@
 import Equipo from "../components/Equipo"
 import AboutUs from "../components/AboutUs"
+import { useLocation } from 'react-router-dom';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from "react";
 
 function Nosotros() {
+  const location = useLocation();
+
+  useEffect(() => {
+    setTimeout(() => {
+      AOS.init();
+      document.querySelectorAll("img").forEach((img) => img.addEventListener("load", () => AOS.refresh()))
+      document.querySelectorAll("video").forEach((video) => video.addEventListener("load", () => AOS.refresh()))
+    }, 1000);
+  }, [location.pathname]);
 
   return (
     <div style={{minHeight: `${window.innerHeight - 116}px` }}>
