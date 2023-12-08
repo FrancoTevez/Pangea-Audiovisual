66a184901d5e97683522ab34f6faaca3a9a72d83
Franco Tevez <tevezzfranco@gmail.com>
2023-12-08 14:52:13 -0300

    arreglando video intro 1

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,26 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Nosotros from './pages/Nosotros'
 import Footer from './containers/Footer'
 import videoLogo from "./assets/logoVideoIntro.mp4"
+import { useEffect, useRef, useState } from 'react'
 
 function App() {
+  const videoRef = useRef(null);
+  const [videoCargado, setVideoCargado] = useState(false);
+
+  useEffect(() => {
+    const handleLoadedData = () => {
+      setVideoCargado(true);
+    };
+
+    if (videoRef.current) {
+      videoRef.current.addEventListener('loadeddata', handleLoadedData);
+    }
+  }, []);
 
   return (
     <BrowserRouter>
-      <div className='logoIntro'>
-        <video src={videoLogo} className='videoIntroPangea' autoPlay muted></video>
+      <div className={videoCargado ? "fondoBlanco logoIntro" : "fondoBlanco"}>
+        <video src={videoLogo} ref={videoRef} className={videoCargado ? "videoIntroPangea" : "displayNoneAll"} autoPlay muted></video>
       </div>
       <Navbar />
       <Routes>
