06a09a837ed273c5568fe12ace81d03db88a8ab0
Franco Tevez <tevezzfranco@gmail.com>
2023-12-13 17:37:24 -0300

    agregando accesibilidad links nosotros

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -3,6 +3,17 @@ import {Link} from "react-router-dom"
 import '../styles/nosotros.css'
 
 function AboutUs() {
+  const scrollToSection = (section) => {
+    setTimeout(() => {
+        const element = document.getElementById(section);
+        console.log(element);
+        element.scrollIntoView({
+            behavior: 'smooth',
+            block: "start",
+            inline: 'start'
+        }); 
+    }, 300);
+  };
 
   return (
     <section className='SectionNosotros'>
@@ -12,7 +23,7 @@ function AboutUs() {
                 <p className='parrafoServicios'>Nuestra satisfacción y deseo más profundo es que el trabajo que realizamos supere tus expectativas. En el afán de lograrlo, descubrimos que la escucha, la comunicación y la empatía son las mejores vías para tal fin.</p>
                 <p className='parrafoServicios'>Pangea nace en 2019 con la iniciativa de empoderar emprendimientos de amigos, mediante la publicidad audiovisual. Así nos fuimos dando cuenta cómo este tipo de contenido impactaba positivamente en cada una de sus audiencias.</p>
                 <p className='parrafoServicios'>Por eso, actualmente elegimos el mundo de las redes sociales: para potenciar el crecimiento de empresas, trabajando junto al área de marketing y paid media.</p>
-                <p className='parrafoServicios'>Agradecemos estar sumergidos en este rubro, porque constantemente nos brinda conocimientos invaluables para elevar la calidad de nuestros <Link to="/#servicios" className='linkToServicios'>servicios.</Link></p>
+                <p className='parrafoServicios'>Agradecemos estar sumergidos en este rubro, porque constantemente nos brinda conocimientos invaluables para elevar la calidad de nuestros <Link to="/" onClick={() => scrollToSection("servicios")} className='linkToServicios'>servicios.</Link></p>
             </div>
         </div>
         <VectorAzul />
--- src/containers/Navbar.jsx
+++ src/containers/Navbar.jsx
@@ -2,9 +2,31 @@ import LinkNosotros from '../components/LinkNosotros'
 import Logo from '../components/Logo'
 import '../styles/navbar.css'
 import { Link } from 'react-scroll'
+import {Link as LinkRouter} from "react-router-dom"
 import NavBurger from './NavBurger'
+import { useLocation } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 
 function Navbar() {
+    const location = useLocation();
+    const [pag, setPag] = useState("")
+
+    useEffect(() => {
+        const rutaActual = location.pathname;
+        setPag(rutaActual)
+      }, [location.pathname]);
+
+      const scrollToSection = (section) => {
+        setTimeout(() => {
+            const element = document.getElementById(section);
+            console.log(element);
+            element.scrollIntoView({
+                behavior: 'smooth',
+                block: "start",
+                inline: 'start'
+            }); 
+        }, 300);
+      };
 
   return (
     <header className='bannerMover'>
@@ -14,16 +36,17 @@ function Navbar() {
                     <Logo />
                 </li>
                 <li>
-                    <Link className='linksHeader' to='servicios' smooth={true} offset={-50} duration={700}>Servicios</Link>
+                    {pag == "/nosotros" ? <LinkRouter onClick={() => scrollToSection("servicios")} className='linksHeader' to='/' >Servicios</LinkRouter> : <Link className='linksHeader' to='servicios' smooth={true} offset={-50} duration={700}>Servicios</Link>}
                 </li>
                 <li>
-                    <Link className='linksHeader' to='portfolio' smooth={true} offset={-20} duration={700}>Portfolio</Link>
+                    {pag == "/nosotros" ? <LinkRouter onClick={() => scrollToSection("portfolio")} className='linksHeader' to='/' >Portfolio</LinkRouter> : <Link className='linksHeader' to='portfolio' smooth={true} offset={-20} duration={700}>Portfolio</Link>}
                 </li>
                 <li>
+
                     <LinkNosotros />
                 </li>
                 <li>
-                    <Link className='linkHCont' to='contacto' smooth={true} offset={-113} duration={700}>Contacto</Link>
+                    {pag == "/nosotros" ? <LinkRouter onClick={() => scrollToSection("contacto")} className='linkHCont' to='/' >Contacto</LinkRouter> : <Link className='linkHCont' to='contacto' smooth={true} offset={-113} duration={700}>Contacto</Link>}
                 </li>
             </ul>
         </nav>
